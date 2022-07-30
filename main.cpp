797d740a98b12a7bac4b65ad6e7e512b1935fb66
Shubham Kumar <developer.shubham07@gmail.com>
2022-07-30 12:37:46 +0530

    adding velocity

--- main.cpp
+++ main.cpp
@@ -5,8 +5,15 @@ int main()
     // window dimensions
     const int windowWidth{512};
     const int windowHeight{380};
+    // Initialize the window
+    InitWindow(windowWidth, windowHeight, "Dasher Dapper Game");
 
-    InitWindow(windowHeight, windowWidth, "Dasher Dapper Game");
+    // rectangle dimensions
+    const int width{50};
+    const int height{80};
+
+    int posY{windowHeight - height};
+    int velocity{0};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -15,7 +22,14 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            velocity -= 10;
+        }
 
+        posY += velocity;
+
+        DrawRectangle(windowHeight/2, posY, width, height, BLUE);
 
         // Stop Drawing
         EndDrawing();
