3a0c5114d96853d25aaf5858bccc4bd8a6dc9e5e
Shubham Kumar <developer.shubham07@gmail.com>
2022-07-30 12:48:00 +0530

    adding gravity and ground condition

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ int main()
     // Initialize the window
     InitWindow(windowWidth, windowHeight, "Dasher Dapper Game");
 
+    // acceleration due to gravity (pixels/frame) / frame
+    const int gravity{1};
+
     // rectangle dimensions
     const int width{50};
     const int height{80};
@@ -22,11 +25,23 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
+        // performing ground check
+        if (posY >= windowHeight - height)
+        {
+            // rectangle is on the ground - do not apply gravity
+            velocity = 0;
+        }
+        else {
+            //apply gravity
+            velocity += gravity;
+        }
+
         if (IsKeyPressed(KEY_SPACE))
         {
             velocity -= 10;
         }
 
+        // update the position
         posY += velocity;
 
         DrawRectangle(windowHeight/2, posY, width, height, BLUE);
