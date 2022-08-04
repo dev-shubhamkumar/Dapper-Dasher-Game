84043a12b26004755db4afea69e544d8f67448d9
Shubham Kumar <developer.shubham07@gmail.com>
2022-08-04 23:30:34 +0530

    Adding AnimData function to update animation

--- main.cpp
+++ main.cpp
@@ -14,6 +14,24 @@ bool isOnGround (AnimData data, int windowHeight)
     return data.pos.y >= windowHeight - data.rec.height;
 };
 
+AnimData updateAnimData(AnimData data, float deltaTime, int maxFrame)
+{
+    // update running time
+    data.runningTime += deltaTime;
+    if (data.runningTime >= data.updateTime)
+    {
+        data.runningTime = 0.0;
+        // update animation frame
+        data.rec.x = data.frame * data.rec.width;
+        data.frame;
+        if (data.frame > maxFrame)
+        {
+            data.frame = 0;
+        }
+    }
+    return data;
+};
+
 
 
 int main()
